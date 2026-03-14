e24d1ba9db2ee10857210481ba68224dee2588bf
ibiscum <ulf.luecking@gmail.com>
2026-03-14 11:25:40 +0100

    ch03

--- Chapter03/2-basic-rest-services/README.md
+++ Chapter03/2-basic-rest-services/README.md
@@ -4,3 +4,13 @@
     java -jar microservices/product-service/build/libs/*.jar &
     curl http://localhost:7002/product/123
     kill $(jobs -p)
+
+    java --enable-native-access=ALL-UNNAMED -jar microservices/product-composite-service/build/libs/*.jar &
+    java --enable-native-access=ALL-UNNAMED -jar microservices/product-service/build/libs/*.jar &
+    java --enable-native-access=ALL-UNNAMED -jar microservices/recommendation-service/build/libs/*.jar &
+    java --enable-native-access=ALL-UNNAMED -jar microservices/review-service/build/libs/*.jar &
+
+    curl http://localhost:7001/product-composite/1
+    curl http://localhost:7001/product-composite/1 -s | jq .
+
+
--- Chapter03/2-basic-rest-services/test-em-all.bash
+++ Chapter03/2-basic-rest-services/test-em-all.bash
@@ -4,18 +4,19 @@
 #
 #   HOST=localhost PORT=7001 ./test-em-all.bash
 #
-: ${HOST=localhost}
-: ${PORT=7001}
-: ${PROD_ID_REVS_RECS=1}
-: ${PROD_ID_NOT_FOUND=13}
-: ${PROD_ID_NO_RECS=113}
-: ${PROD_ID_NO_REVS=213}
+: "${HOST=localhost}"
+: "${PORT=7001}"
+: "${PROD_ID_REVS_RECS=1}"
+: "${PROD_ID_NOT_FOUND=13}"
+: "${PROD_ID_NO_RECS=113}"
+: "${PROD_ID_NO_REVS=213}"
 
 function assertCurl() {
 
   local expectedHttpCode=$1
   local curlCmd="$2 -w \"%{http_code}\""
-  local result=$(eval $curlCmd)
+  local result
+  result=$(eval "$curlCmd")
   local httpCode="${result:(-3)}"
   RESPONSE='' && (( ${#result} > 3 )) && RESPONSE="${result%???}"
 
@@ -57,9 +58,9 @@ echo "PORT=${PORT}"
 
 # Verify that a normal request works, expect three recommendations and three reviews
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
-assertEqual $PROD_ID_REVS_RECS $(echo $RESPONSE | jq .productId)
-assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual "$PROD_ID_REVS_RECS" "$(echo $RESPONSE | jq .productId)"
+assertEqual 3 "$(echo "$RESPONSE" | jq ".recommendations | length")"
+assertEqual 3 "$(echo "$RESPONSE" | jq ".reviews | length")"
 
 # Verify that a 404 (Not Found) error is returned for a non-existing productId ($PROD_ID_NOT_FOUND)
 assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
@@ -67,22 +68,22 @@ assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPON
 
 # Verify that no recommendations are returned for productId $PROD_ID_NO_RECS
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_RECS -s"
-assertEqual $PROD_ID_NO_RECS $(echo $RESPONSE | jq .productId)
-assertEqual 0 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual "$PROD_ID_NO_RECS" "$(echo "$RESPONSE" | jq .productId)"
+assertEqual 0 "$(echo "$RESPONSE" | jq ".recommendations | length")"
+assertEqual 3 "$(echo "$RESPONSE" | jq ".reviews | length")"
 
 # Verify that no reviews are returned for productId $PROD_ID_NO_REVS
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_REVS -s"
-assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | jq .productId)
-assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 0 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual "$PROD_ID_NO_REVS" "$(echo "$RESPONSE" | jq .productId)"
+assertEqual 3 "$(echo "$RESPONSE" | jq ".recommendations | length")"
+assertEqual 0 "$(echo "$RESPONSE" | jq ".reviews | length")"
 
 # Verify that a 422 (Unprocessable Entity) error is returned for a productId that is out of range (-1)
 assertCurl 422 "curl http://$HOST:$PORT/product-composite/-1 -s"
-assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
+assertEqual "\"Invalid productId: -1\"" "$(echo "$RESPONSE" | jq .message)"
 
 # Verify that a 400 (Bad Request) error error is returned for a productId that is not a number, i.e. invalid format
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
-assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
+assertEqual "\"Type mismatch.\"" "$(echo "$RESPONSE"   | jq .message)"
 
-echo "End, all tests OK:" `date`
+echo "End, all tests OK:" "$(date)"
